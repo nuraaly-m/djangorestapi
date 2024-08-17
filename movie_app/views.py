d762bd9289fcb544b0f2dc06d81e9504fd6ce859
nuraaly <nuraalymelisbekv67@gmail.com>
2024-08-17 19:40:13 +0600

    1hw

---
+++ movie_app/views.py
@@ -0,0 +1,56 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Director, Movie, Review
+from rest_framework import status
+from .serializers import DirectorSerializer, MovieSerializer, ReviewSerializer
+
+
+@api_view(['GET'])
+def director_detail_api_view(request, id):
+    try:
+        director = Director.objects.get(id=id)
+    except:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    data = DirectorSerializer(director).data
+    return Response(data=data)
+
+
+@api_view(http_method_names=['GET'])
+def director_list_api_view(request):
+    director = Director.objects.all()
+    list_ = DirectorSerializer(instance=director, many=True).data
+    return Response(data=list_, status=status.HTTP_200_OK)
+
+
+@api_view(['GET'])
+def movie_detail_api_view(request, id):
+    try:
+        movie = Movie.objects.get(id=id)
+    except:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    data = MovieSerializer(movie).data
+    return Response(data=data)
+
+
+@api_view(http_method_names=['GET'])
+def movie_list_api_view(request):
+    movie = Movie.objects.all()
+    list_ = MovieSerializer(instance=movie, many=True).data
+    return Response(data=list_, status=status.HTTP_200_OK)
+
+
+@api_view(['GET'])
+def review_detail_api_view(request, id):
+    try:
+        review = Review.objects.get(id=id)
+    except:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    data = ReviewSerializer(review).data
+    return Response(data=data)
+
+
+@api_view(http_method_names=['GET'])
+def review_list_api_view(request):
+    review = Review.objects.all()
+    list_ = ReviewSerializer(instance=review, many=True).data
+    return Response(data=list_, status=status.HTTP_200_OK)
